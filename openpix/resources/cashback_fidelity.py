2b85ac1b144996deac4b7b573be597353ab48e00
eletroswing <ytalosilvabatalha@gmail.com>
2024-05-07 20:56:23 -0300

    add  resource

--- openpix/resources/cashback_fidelity.py
+++ openpix/resources/cashback_fidelity.py
@@ -2,6 +2,7 @@
     Module: cashback_fidelity
 """
 from openpix.http import HttpClient
+import cashback_fidelity_types
 
 class CashbackFidelity:
     """
@@ -12,8 +13,19 @@ class CashbackFidelity:
     def __init__(self, HttpClient: HttpClient):
         self._client = HttpClient
     
-    def get(self):
-        return
+    """Get one fidelity.
+    Args:
+        taxID (str): the user's tax id document
+        [Click here for m   ore info](https://developers.woovi.com/api#tag/cashback-fidelity/paths/~1api~1v1~1cashback-fidelity~1balance~1%7BtaxID%7D/get)
+    """
+    def get(self, taxID: str) -> cashback_fidelity_types.CashbackGet:
+        return self._client.get(path=f'/api/v1/cashback-fidelity/balance/{taxID}')
 
-    def create(self):
-        return
+    """Create one fidelity.
+    Args:
+        taxID (str): the user's tax id document
+        value (int): the fidelity value
+        [Click here for more info](https://developers.woovi.com/api#tag/cashback-fidelity/paths/~1api~1v1~1cashback-fidelity/post)
+    """
+    def create(self, taxID: str, value: int) -> cashback_fidelity_types.CashbackCreate:
+        return self._client.post(path=f'/api/v1/cashback-fidelity', data={value: value, taxID: taxID})
---
+++ openpix/resources/cashback_fidelity_types.py
@@ -0,0 +1,15 @@
+from openpix.types import PageInfo
+
+class CashbackInfo:
+    def __init__(self, value: int):
+        self.value: value
+
+class CashbackGet:
+    def __init__(self, balance: int, status: str):
+        self.balance = balance
+        self.status = status
+
+class CashbackCreate:
+    def __init__(self, cashback: CashbackInfo, message: str):
+        self.cashback = cashback
+        self.message = message
