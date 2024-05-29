0652e88e1a2a6c0f3ca2a2a81af05c6b8d7db535
eletroswing <ytalosilvabatalha@gmail.com>
2024-05-29 19:25:54 -0300

    fix(customer): adding docs for customer

--- openpix/resources/customer.py
+++ openpix/resources/customer.py
@@ -14,12 +14,44 @@ class Customer:
     def __init__(self, HttpClient: HttpClient):
         self._client = HttpClient
 
+    """get a customer
+    Args:
+        id (str): identifier of your customer.
+
+        [Click here for more info](https://developers.openpix.com.br/api#tag/customer/paths/~1api~1v1~1customer~1%7Bid%7D/get)
+    """
     def get(self, id: str) -> charge_types.CustomerGet:
         return self._client.get(path=f'/api/v1/customer/{id}')
 
+    """list customers
+    Args:
+        limit (int, optional): itens per page, defaults 10.
+        skip (int, optional): how many itens are gonna be skipped, default 0.
+    
+        [Click here for more info](https://developers.openpix.com.br/api#tag/customer/paths/~1api~1v1~1customer~1%7Bid%7D/get)
+    """
     def list(self, limit: int = 10, skip: int = 0) -> charge_types.ChargeList:
         return self._client.get(path=f'/api/v1/customer', query={"limit": limit, "skip": skip})
 
+    """create a customer
+    Args:
+        name (str): name of the customer.
+        email (str): email of the customer.
+        phone (str): phone number of your customer.
+        taxID (str): personal document of your customer.
+        correlationID (str): Your correlation ID, unique identifier refund.
+        address (Address): 
+            - zipcode: string
+            - street: string
+            - number: string
+            - neighborhood: string
+            - city: string
+            - state: string
+            - complement: string
+            - country: string
+    
+        [Click here for more info](https://developers.openpix.com.br/api#tag/customer/paths/~1api~1v1~1customer/post)
+    """
     def create(self, name: str, email: str, phone: str, taxID: str, address: charge_types.Address) -> charge_types.CustomerCreate:
         return self._client.post(path=f'/api/v1/customer', data={
             'name': name,
@@ -38,6 +70,25 @@ class Customer:
             }
         })
     
+    """update a customer
+    Args:
+        correlationID (str): Your correlation ID, unique identifier refund.
+        name (str): name of the customer.
+        email (str): email of the customer.
+        phone (str): phone number of your customer.
+        taxID (str): personal document of your customer.
+        address (Address): 
+            - zipcode: string
+            - street: string
+            - number: string
+            - neighborhood: string
+            - city: string
+            - state: string
+            - complement: string
+            - country: string
+    
+        [Click here for more info](https://developers.openpix.com.br/api#tag/customer/paths/~1api~1v1~1customer/post)
+    """
     def update(self, correlationID: str, name: str, email: str, phone: str, taxID: str, address: charge_types.Address) -> charge_types.CustomerUpdate:
         return self._client.patch(path=f'/api/v1/customer/{correlationID}', data={
             'name': name,
